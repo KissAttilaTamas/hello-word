f1ef1856791e36a4264981f1326e0a94219c3f31
KissAttilaTamas <KissAttilaTamas@googlemail.com>
2016-11-11 18:00:14 +0000

    fibanocci

--- whileciklus01.py
+++ whileciklus01.py
@@ -1,6 +1,12 @@
-a=1
-while a<20:
-          print(a,". elem",a*7 \n");
-          a=a+1;
-          b=a;
-          
+# Első Python script
+# Fibonacci-sort írat ki, azaz egy olyan számsort, aminek minden tagja
+# az előző két tag összege.
+# a & b az egymást követő tagok számolására valók # c egy számláló
+# az első tag kiíratása
+# összesen 15 tagot íratunk ki
+
+a, b, c = 1, 1, 1
+print (1)
+while c<15:
+            a, b, c = b, a+b, c+1
+            print (b)
