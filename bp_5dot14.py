e999ef6ca2b01c88e445ff876f18a1e1cbfb1217
KissAttilaTamas <KissAttilaTamas@googlemail.com>
2016-11-15 13:25:44 +0000

    feladat 6

---
+++ bp_5dot14.py
@@ -0,0 +1,12 @@
+t1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+t2 = ['Január', 'Február', 'Március', 'Április', 'Május', 'Június','Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December']
+x=0
+t11, t12=[],[]
+print(t2[x])
+while x<len(t2):
+    if x % 2== 0:
+        t11.append(t2[x])
+    else:
+        t12.append(t2[x])
+    x=x+1
+print(t2,"\n",t11,"\n",t12) 
---
+++ bp_6dot01.py
@@ -0,0 +1,11 @@
+def szorzoTabla(alap, kezdete, vege):
+    print ('Az', alap, '-ös szorzótábla :')
+    n = kezdete
+    while n <= vege :
+        print( n, 'x', alap, '=',n * alap)
+        n = n +1
+
+szorzoTabla(10,2,7)
+
+
+
